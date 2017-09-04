3b951bcf897d022cd6b023ddf0dd09fe7b14e111
Deepthi Gunasekara <DGunasekara@users.noreply.github.com>
2017-09-04 19:51:36 +0530

    commit for Array - Printing the elements in an array

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Array{
+
+	public static void main(String[] args){
+
+		String[] name = {"superman","spiderman","abc","123","efg","456"};
+		String[] pass = {"abc","123","efg","456"};
+
+		for(int i=0;i<name.length;i++){
+			System.out.println(name[i]);
+		}
+	}
+}
