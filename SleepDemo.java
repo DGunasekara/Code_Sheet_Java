f305450d8889c0edeb057027c3ddef20f09b8650
Deepthi Gunasekara <DGunasekara@users.noreply.github.com>
2017-09-05 08:54:53 +0530

    create SleepDemo.java

---
+++ SleepDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SleepDemo{
+	public static void main(String[] args){
+		try{
+			System.out.println(new Date()+"\n");
+	
+			Thread.sleep(5*60*10);
+
+			System.out.println(new Date());
+		}catch(Exception e){
+			System.out.println("Got an Exception");
+		}
+	}
+}
